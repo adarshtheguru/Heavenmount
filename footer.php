79de163408517ce839c437c79108d6dcf52ad664
Adarsh <adarsh.jaiswal@beyondwalls.com>
2023-06-22 22:11:19 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer>
+		<div class="row">
+			<div class="col-12 lower">
+				<div class="container">
+					<div class="row">
+						<div class="col-7 col-xs-12">
+							<p>&copy; <span class="copyrightYear"></span>&nbsp;All Rights Reserved</p>
+						</div>
+						<div class="col-5 col-xs-12 text-right">
+							<p>Created By: <a href="https://github.com/adarshtheguru" target="_blank">AjaiX</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<div class="mob-footer visible-xs">
+		<div class="row">
+			<div class="col-6">
+				<a href="tel:+911234567890" class="mob-call-btn"><span class="sell_do_virtual_numbers">+91 1234 5678 90</span></a>
+			</div>
+			<div class="col-6">
+				<a href="javascript:;" class="mob_enq_click mob-enq-btn" data-event-category="Footer" data-event-action="Click" data-event-name="Enquire Now">Enquire Now</a>
+			</div>
+		</div>
+	</div>
+	<script src="js/jquery.js"></script>
+	<script src="js/slick.js"></script>
+	<script src="js/app.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include "header.php" ?>
+	<section class="section-first" id="link1">
+		<div class="row">
+			<div class="col-12 col-xs-12 banner_ht bg-cover home-banner">
+			</div>
+		</div>
+	</section>
+
+	<section class="padding" id="link2">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 banner_ht">
+					<p>Section 2</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="padding" id="link3">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 banner_ht">
+					<p>Section 3</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section class="padding" id="link4">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 banner_ht">
+					<p>Section 4</p>
+				</div>
+			</div>
+		</div>
+	</section>
+<?php include "footer.php" ?>
